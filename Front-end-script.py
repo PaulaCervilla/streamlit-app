a973b77ce65802b80b2ab2bdd342d9f3fada4fd1
Paula Cervilla <paulacervillagarcia@gmail.com>
2022-01-19 19:36:40 +0100

    updating script

--- Front-end-script.py
+++ Front-end-script.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from sklearn.metrics import mean_squared_error
 from math import sqrt
-from statsmodels.tsa.arima_model import ARIMAResults
 import datetime
 import re
 import base64
